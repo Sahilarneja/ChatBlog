436973b88a61c9ea4705be0906c3d7c0e1b2ab32
sahil <sahilarneja2003@gmail.com>
2024-06-16 20:03:23 +0530

    contacts feature added

--- public/src/pages/Chat.jsx
+++ public/src/pages/Chat.jsx
@@ -1,21 +1,30 @@
 import React, { useState, useEffect } from 'react';
-import styled from "styled-components";
-import axios from "axios";
+import styled from 'styled-components';
+import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import { allUsersRoutes } from '../utils/APIRoutes';
 import Contacts from '../components/Contacts';
+import Welcome from '../components/Welcome';
 
 const Chat = () => {
   const navigate = useNavigate();
   const [contacts, setContacts] = useState([]);
   const [currentUser, setCurrentUser] = useState(undefined);
+  const [currentChat, setCurrentChat] = useState(undefined);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchCurrentUser = async () => {
-      if (!localStorage.getItem("chat-app-user")) {
-        navigate("/login");
-      } else {
-        setCurrentUser(JSON.parse(localStorage.getItem("chat-app-user")));
+      try {
+        const user = localStorage.getItem('chat-app-user');
+        if (!user) {
+          navigate('/login');
+        } else {
+          setCurrentUser(JSON.parse(user));
+        }
+      } catch (error) {
+        console.error('Error fetching user from localStorage:', error);
+        navigate('/login');
       }
     };
     fetchCurrentUser();
@@ -24,21 +33,36 @@ const Chat = () => {
   useEffect(() => {
     const fetchContacts = async () => {
       if (currentUser) {
-        if (currentUser.isAvatarImageSet) {
-          const { data } = await axios.get(`${allUsersRoutes}/${currentUser._id}`);
-          setContacts(data);
-        } else {
-          navigate("/setAvatar");
+        try {
+          if (currentUser.isAvatarImageSet) {
+            const { data } = await axios.get(`${allUsersRoutes}/${currentUser._id}`);
+            setContacts(data.users);
+            setLoading(false);
+          } else {
+            navigate('/setAvatar');
+          }
+        } catch (error) {
+          console.error('Error fetching contacts:', error);
+          // Handle error (e.g., navigate to an error page)
         }
       }
     };
     fetchContacts();
   }, [currentUser, navigate]);
 
+  const handleChatChange = (chat) => {
+    setCurrentChat(chat);
+  };
+
+  if (loading) {
+    return <LoadingScreen>Loading...</LoadingScreen>;
+  }
+
   return (
     <Container>
       <div className='container'>
-        <Contacts contacts={contacts} />
+        <Contacts contacts={contacts} currentUser={currentUser} changeChat={handleChatChange} />
+        <Welcome currentUser={currentUser} />
       </div>
     </Container>
   );
@@ -46,7 +70,7 @@ const Chat = () => {
 
 const Container = styled.div`
   height: 100vh;
-  width: 100vw; /* Corrected to vw */
+  width: 100vw;
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -56,15 +80,24 @@ const Container = styled.div`
 
   .container {
     height: 85vh;
-    width: 85vw; /* Corrected to vw */
+    width: 85vw;
     background-color: #00000076;
     display: grid;
     grid-template-columns: 25% 75%;
-    
+
     @media screen and (min-width: 720px) and (max-width: 1024px) {
       grid-template-columns: 35% 65%;
     }
   }
+`;
+
+const LoadingScreen = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 2rem;
+  color: #fff;
 `;
 
 export default Chat;
